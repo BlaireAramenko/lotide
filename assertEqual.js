a3f5d8f2edfc43e581f2ecc6da89a809bb62ed9c
BlaireAramenko <blaire.aramenko21@gmail.com>
2023-02-07 21:53:25 -0800

    Updated assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👍 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`👎 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+let array = [];
+const head = function(array) {
+
+  return array[0];
+};
+
+assertEqual(head(2), 2);
