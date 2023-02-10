d8b5db342f476ce7cff4b1995fc8c50e2cdfcc1c
BlaireAramenko <blaire.aramenko21@gmail.com>
2023-02-09 16:14:32 -0800

    Added countLetters

---
+++ countLetters.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👍 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`👎 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+let array = [];
+const head = function(array) {
+
+  return array[0];
+};
+
+const countLetters = function(str) {
+  let counter = {};
+
+  for (let value of str) {
+    if (value === ' ') {
+      continue;
+    } else {
+      counter[value] = (counter[value] || 0) + 1;
+    }
+  }
+  return counter;
+};
+countLetters("Hello World");
+console.log(countLetters("Hello World"));
+
+module.exports = countLetters;
