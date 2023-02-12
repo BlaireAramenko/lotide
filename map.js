17bdd361262cca681d4f7908df1c78ac4da278c0
BlaireAramenko <blaire.aramenko21@gmail.com>
2023-02-12 01:13:45 -0800

    Added map.js

---
+++ map.js
@@ -0,0 +1,39 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👍 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`👎 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+let array = [];
+const head = function(array) {
+
+  return array[0];
+};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) return false;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) return false;
+  }
+  return true;
+};
+
+eqArrays([1, 2, 3], [1, 2, 3]);
+eqArrays([1, 2, 3], [3, 2, 1]);
+eqArrays(["1", "2", "3"], ["1", "2", "3"]);
+eqArrays(["1", "2", "3"], ["1", "2", 3]);
+
+
+const words = ["dog", "cat", "monkey", "elephant", "tiger"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
