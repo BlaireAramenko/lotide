2e6d2d523b77f69960448fc6b3a8c217c96d5c91
BlaireAramenko <blaire.aramenko21@gmail.com>
2023-02-14 13:04:27 -0800

    edited

--- index.js
+++ index.js
@@ -2,9 +2,16 @@
 const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
+const assertEqual = require('./assertEqual');
+const assertArraysEqual = require('./assertArraysEqual');
+const eqArrays = require('./eqArrays');
 
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle
+  head,   
+  tail,
+  middle,
+  assertEqual,
+  assertArraysEqual,
+  eqArrays
+
 };
