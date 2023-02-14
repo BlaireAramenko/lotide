264b62090c8953186d48d36d5b8e391ea2392497
BlaireAramenko <blaire.aramenko21@gmail.com>
2023-02-13 21:19:50 -0800

    edited

--- map.js
+++ map.js
@@ -1,24 +1,35 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👍 Assertion Passed: ${actual} === ${expected}`);
+const assertEqual = function(array1, array2) {
+
+  if (array1 !== array2) {
+    console.log(`💔 Assertion Failed: ${array1} !== ${array2}`);
   } else {
-    console.log(`👎 Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`💕 Assertion Passed: ${array1} === ${array2}`);
   }
 
 };
 
-let array = [];
 const head = function(array) {
 
   return array[0];
 };
 
 const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) return false;
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) return false;
+
+  if (array1.length !== array2.length) {
+    return false;
   }
+
+  for(let i = 0; i < array1.length; i++) {
+
+    if (array1[i] !== array2[i]){
+      return false;
+
+    }
+
+  }
+
   return true;
+  
 };
 
 eqArrays([1, 2, 3], [1, 2, 3]);
