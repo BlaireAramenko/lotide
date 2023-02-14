a8d4e5985ead856716cf1ead994599a19ade14e7
BlaireAramenko <blaire.aramenko21@gmail.com>
2023-02-13 21:21:28 -0800

    edited

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,48 +1,4 @@
-/* const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👍 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`👎 Assertion Failed: ${actual} !== ${expected}`);
-  }
-
-};
-
-let array = [];
-const head = function(array) {
-
-  return array[0];
-};
-
-const findKeyByValue = function(obj, val) {
-  let objKeys = Object.keys(obj);
-  let index = 0;
-  for (let element in obj) {
-    if (obj[element] === val) {
-      return objKeys[index];
-    }
-    index++;
-  }
-  return undefined;
-}
-
-
-
-const bestTVShowsByGenre = { 
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
-
-//   assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-//   assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
-console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"));
-console.log(findKeyByValue(bestTVShowsByGenre, "That '70s Show"));
-
-module.exports = findKeyByValue;
-
-*/
-
-//instructor answer
+//adjusted answer based on instructor Gary's answer
 
 
 const findKeyByValue = function(obj, value) {
