4723b8d890bb75495f9b394cc4adf6dfa5b44c18
BlaireAramenko <blaire.aramenko21@gmail.com>
2023-02-13 21:29:08 -0800

    edited

--- letterPositions.js
+++ letterPositions.js
@@ -1,60 +1,41 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👍 Assertion Passed: ${actual} === ${expected}`);
+const assertEqual = function(array1, array2) {
+
+  if (array1 !== array2) {
+    console.log(`💔 Assertion Failed: ${array1} !== ${array2}`);
   } else {
-    console.log(`👎 Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`💕 Assertion Passed: ${array1} === ${array2}`);
   }
 
 };
 
-let array = [];
 const head = function(array) {
 
   return array[0];
 };
 
 const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) return false;
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) return false;
+
+  if (array1.length !== array2.length) {
+    return false;
   }
+
+  for(let i = 0; i < array1.length; i++) {
+
+    if (array1[i] !== array2[i]){
+      return false;
+
+    }
+
+  }
+
   return true;
+  
 };
 
 eqArrays([1, 2, 3], [1, 2, 3]);
 eqArrays([1, 2, 3], [3, 2, 1]);
 eqArrays(["1", "2", "3"], ["1", "2", "3"]);
 eqArrays(["1", "2", "3"], ["1", "2", 3]);
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👍 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`👎 Assertion Failed: ${actual} !== ${expected}`);
-  }
-
-};
-
-let array = [];
-const head = function(array) {
-
-  return array[0];
-};
-
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) return false;
-
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) return false;
-  }
-
-  return true;
-};
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
 
 const letterPositions = function(sentence) {
   const results = {};
@@ -76,4 +57,3 @@ console.log(letterPositions('I am Web Developer'))
 
 assertArraysEqual(letterPositions("hello").e, [1]);
 
-module.exports = letterPositions;
