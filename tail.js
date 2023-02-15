7a673f90cc0fdcde31fe040f70d3f435c8e7b6a8
Blaire Aramenko <90530745+BlaireAramenko@users.noreply.github.com>
2023-02-14 16:00:38 -0800

    Update tail.js

--- tail.js
+++ tail.js
@@ -1,18 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👍 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`👎 Assertion Failed: ${actual} !== ${expected}`);
-  }
-
-};
-
-let array = [];
-const head = function(array) {
-
-  return array[0];
-};
-
 function tail(arr) {
   return arr.slice(1);
 }
